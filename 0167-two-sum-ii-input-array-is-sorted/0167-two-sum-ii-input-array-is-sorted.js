b706557e82707062dc14bfbbcf25eeb8919b7e6d
Pikadev1771 <111509842+Pikadev1771@users.noreply.github.com>
2023-03-10 16:41:10 +0900

    Time: 103 ms (8.29%), Space: 43.2 MB (32.51%) - LeetHub

---
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+    
+    
+    // for (let i = 0; i< numbers.length; i++) {
+    //     for (let j = 1; i< numbers.length; j++) {
+    //         if(numbers[i]+numbers[j] === target) {
+    //             return [i+1, j+1]
+    //         }
+    //     }
+
+    // }
+
+    let idx1 = 0;
+    let idx2 = numbers.length - 1;
+
+    while ( idx1 < idx2 ) {
+        const sum = numbers[idx1] + numbers[idx2];
+       if ( target === sum) {
+           return [idx1+1, idx2+1];
+       } else if ( sum > target ) {
+           idx2--;
+       } else if ( sum < target ) {
+           idx1++;
+       }
+    }
+
+
+
+};
