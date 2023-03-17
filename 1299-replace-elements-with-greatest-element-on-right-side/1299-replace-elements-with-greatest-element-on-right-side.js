6dbcb728bdc92305965b810fbada5639dba829db
Pikadev1771 <111509842+Pikadev1771@users.noreply.github.com>
2023-03-17 15:11:11 +0900

    Time: 175 ms (83.92%), Space: 51.3 MB (56.75%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.js
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.js
@@ -4,12 +4,12 @@
  */
 var replaceElements = function(arr) {
 
-  let grtst = -1
+  let max = -1
   
   for (let i = arr.length-1; i>= 0; i--) {
-      let num = arr[i] // 2. 비교 위해 원래의 수 keep
-      arr[i] = grtst // 3. 가장 큰 수로 갈아끼움
-      grtst = Math.max(grtst, num) // 1. 가장 큰 수 만들어놓음
+      let temp = arr[i] // 2. 비교 위해 원래의 수 keep
+      arr[i] = max // 3. 가장 큰 수로 갈아끼움
+      max = Math.max(max, temp) // 1. 가장 큰 수 만들어놓음
   }
 
 return arr
