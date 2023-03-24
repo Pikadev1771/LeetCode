1b0406d65383ce4f7a4209971b78c7b37643aa03
Pikadev1771 <111509842+Pikadev1771@users.noreply.github.com>
2023-03-24 10:56:35 +0900

    Time: 67 ms (99.52%), Space: 44.1 MB (79.85%) - LeetHub

---
+++ 0018-4sum/0018-4sum.js
@@ -0,0 +1,41 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[][]}
+ */
+var fourSum = function(nums, target) {
+    let result = []
+    nums.sort((a, b) => a - b)
+
+    
+
+    for(let a = 0; a < nums.length - 3; a++) {
+         if(a > 0 && nums[a] === nums[a-1]) continue;
+        for(let b = a+1; b < nums.length - 2; b++) {
+             if(b > a+1 && nums[b] === nums[b-1]) continue;
+            let c = b+1
+            let d = nums.length-1
+
+            while(c < d) {
+                let sum = nums[a] + nums[b] + nums[c] + nums[d]
+          
+                if(sum === target) {
+                result.push([nums[a], nums[b], nums[c], nums[d]])
+
+                while(nums[c] === nums[c+1]) c++
+                while(nums[d] === nums[d-1]) d--
+
+                c++
+                d--
+            } else if (sum < target) {
+                c++
+            } else {
+                d--
+            }
+        }
+        }
+    }
+    return result
+}
+
+
