372a1a04468c8f9c73e7370e9b60d80e115a8852
Pikadev1771 <111509842+Pikadev1771@users.noreply.github.com>
2023-03-24 20:45:28 +0900

    Added solution - LeetHub

---
+++ Sort an array of 0s, 1s and 2s - GFG/sort-an-array-of-0s-1s-and-2s.js
@@ -0,0 +1,96 @@
+//{ Driver Code Starts
+//Initial Template for javascript
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/* Function to print an array */
+function printArray(arr, size)
+{
+    let i;
+    let s='';
+    for (i=0; i < size; i++) {
+        s += arr[i] + " ";
+    }
+    console.log(s);
+}
+
+function main() {
+    let t = parseInt(readLine());
+    let i = 0;
+    for(;i<t;i++)
+    {
+        let input_ar0 = readLine().split(' ').map(x=>parseInt(x));
+        let n = input_ar0[0];
+        let arr = new Array(n);
+        let input_ar1 = readLine().split(' ').map(x=>parseInt(x));
+        for(let i=0;i<n;i++)
+            arr[i] = input_ar1[i];
+        let obj = new Solution();
+        obj.sort012(arr, n);
+        printArray(arr, arr.length);
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for javascript
+
+/**
+ * @param {number[]} arr
+ * @param {number} N
+ * @returns {number[]}
+*/
+
+class Solution {
+    
+    sort012(arr, N)
+    {
+        let zeros = 0;
+        let ones = 0;
+        let twos = 0;
+
+        for (let i=0; i<arr.length; i++) {
+             if(arr[i] === 0) {
+                 zeros++ //2
+             } else if (arr[i] === 1) {
+                ones++ //1
+            } else {
+                twos++  //2
+           }
+        }
+     
+    for (let i=0; i<arr.length; i++) {
+        if(i < zeros ) {
+            arr[i] = 0
+        } else if (i >= zeros && i < zeros + ones) {
+            arr[i] = 1
+        } else {
+            arr[i] = 2
+        }
+     
+    }
+    
+}
+}
