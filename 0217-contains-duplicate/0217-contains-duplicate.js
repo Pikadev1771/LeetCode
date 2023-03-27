0f66ae3382a2c7bca54b40f41e9410784b0eb1e2
Pikadev1771 <111509842+Pikadev1771@users.noreply.github.com>
2023-03-27 19:51:45 +0900

    Time: 155 ms (29.09%), Space: 51.8 MB (87.28%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -4,13 +4,7 @@
  */
 var containsDuplicate = function(nums) {
 
-    // for(el of nums) {
-    //     if(nums.indexOf(el) !== nums.lastIndexOf(el)) {
-    //         return true
-    //     }
-    // }
-    // return false
-    
+
     nums.sort((a, b) => a - b)
     for (let i = 0; i< nums.length -1; i++) {
         if(nums[i] === nums[i+1]) {
