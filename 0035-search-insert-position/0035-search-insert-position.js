c5d1330a81fe4bff08299f9cb22a5a65ed5be520
Pikadev1771 <111509842+Pikadev1771@users.noreply.github.com>
2023-04-05 09:56:10 +0900

    Time: 57 ms (78.71%), Space: 41.5 MB (92.98%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.js
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -4,10 +4,16 @@
  * @return {number}
  */
 var searchInsert = function(nums, target) {
+
+    if(nums[nums.length -1] < target) {
+        return nums.length
+    }
+
+
     for (let i = 0; i < nums.length; i++) {
         if(nums[i] >= target) {
             return i
       } 
     }
-    return nums.length
+    // return nums.length
 };
