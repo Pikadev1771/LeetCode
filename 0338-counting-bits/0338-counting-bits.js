623fd628fdd13f05736d4e3886610ff2e1a281b0
Pikadev1771 <111509842+Pikadev1771@users.noreply.github.com>
2023-04-10 10:01:56 +0900

    Time: 69 ms (98.47%), Space: 47.7 MB (86.55%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+var countBits = function(n) {
+    let result = Array(n+1).fill(0)
+    let offset = 1;
+
+    for(let i = 1; i <= n; i++) {
+        if(offset * 2 === i) {
+            offset = i
+        }
+        result[i] = result[i - offset] + 1
+    }
+
+    return result
+
+
+};
