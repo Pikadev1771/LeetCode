4f5d20bde4f48d0b6638d72f699020be4fd379f8
Pikadev1771 <111509842+Pikadev1771@users.noreply.github.com>
2023-05-04 09:09:50 +0900

    Time: 99 ms (6.33%), Space: 41.8 MB (52.20%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.js
+++ 0509-fibonacci-number/0509-fibonacci-number.js
@@ -3,16 +3,8 @@
  * @return {number}
  */
 var fib = function(n) {
-    let memo = [0, 1]
-
-    function fibNum(num) {
-        if(memo[num] !== undefined) {
-            return memo[num]
-        }
-        memo[num] = fibNum(num-1) + fibNum(num-2)
-        return memo[num]
-    }
-
-    return fibNum(n)
+    if(n <= 1) return n;
+    
+    return fib(n-1) + fib(n-2)
     
 };
