389f823a90e870fbe7e5fe541359f2b5d372bae5
Pikadev1771 <111509842+Pikadev1771@users.noreply.github.com>
2023-05-16 11:50:50 +0900

    Time: 59 ms (55.87%), Space: 41.9 MB (74.26%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.js
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -5,15 +5,21 @@
  */
 var searchInsert = function(nums, target) {
 
-    if(nums[nums.length -1] < target) {
-        return nums.length
-    }
+  let start = 0;
+  let end = nums.length - 1;
+  let answer = nums.length; // target이 end보다 클 경우
 
+  while(start <= end) {
+      const mid = Math.floor((start + end)/ 2); // 중간 인덱스 혹은 중간-1 인덱스
 
-    for (let i = 0; i < nums.length; i++) {
-        if(nums[i] >= target) {
-            return i
-      } 
-    }
-    // return nums.length
+      if(nums[mid] === target) {
+          return mid 
+      } else if (nums[mid] < target) { 
+          start = mid + 1
+      } else { //  타겟이 중간값보다 작은 경우 
+          answer = mid;
+          end = mid - 1;
+      }
+  }
+  return answer;
 };
