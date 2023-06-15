9dcfbdea17b5767f806ccb6a01c54e2d17853654
Pikadev1771 <111509842+Pikadev1771@users.noreply.github.com>
2023-06-15 10:25:00 +0900

    Time: 49 ms (99.33%), Space: 42.6 MB (50.66%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -7,9 +7,11 @@ var twoSum = function(nums, target) {
    const obj = {}
 
    for(let i = 0; i < nums.length; i++) {  
-       if(obj[target-nums[i]] != undefined) {
+       if(obj[target-nums[i]] !== undefined) {
            return [obj[target-nums[i]], i] // 인덱스 배열을 리턴
        }
        obj[nums[i]] = i;
    }    
+
+
 };
